8c13983e02e4fc6a8d76e338d8785d4ed26835f6
angela-vargas <u01a7094@lacaixa.es>
2024-12-20 13:27:27 +0100

    Adding date

--- www/js/welcome.js
+++ www/js/welcome.js
@@ -1,3 +1,35 @@
+// Establece la fecha objetivo
+const fechaObjetivo = new Date("May 31, 2025 13:00:00").getTime();
+
+// Actualiza la cuenta regresiva cada 1 segundo
+const x = setInterval(function() {
+
+  // Obtén la fecha y hora actual
+  const ahora = new Date().getTime();
+
+  // Encuentra la distancia entre ahora y la fecha objetivo
+  const distancia = fechaObjetivo - ahora;
+
+  // Calcula el tiempo para días, horas, minutos y segundos
+  const dias = Math.floor(distancia / (1000 * 60 * 60 * 24));
+  const horas = Math.floor((distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutos = Math.floor((distancia % (1000 * 60 * 60)) / (1000 * 60));
+  const segundos = Math.floor((distancia % (1000 * 60)) / 1000);
+
+  // Muestra el resultado en los elementos con los IDs correspondientes
+  document.getElementById("dias").innerHTML = dias;
+  document.getElementById("horas").innerHTML = horas;
+  document.getElementById("minutos").innerHTML = minutos;
+  document.getElementById("segundos").innerHTML = segundos;
+
+  // Si la cuenta regresiva ha terminado, escribe un mensaje
+  if (distancia < 0) {
+    clearInterval(x);
+    document.getElementById("cuenta-atras").innerHTML = "¡La cuenta regresiva ha terminado!";
+  }
+}, 1000);
+
+
 // Función para mostrar Google Maps
 function initMap() {
   const myLatLng = {
